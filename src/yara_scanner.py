02598c26c68378ebef0c46a2362849beaca95c6f
Priya-2910 <231901025@rajalakshmi.edu.in>
2025-05-23 20:34:19 +0530

    Delete src directory

--- src/yara_scanner.py
+++
@@ -1,6 +0,0 @@
-import yara
-
-def scan_file_with_yara(file_path, rules_path="rules/fake_rules.yar"):
-    rules = yara.compile(filepath=rules_path)
-    matches = rules.match(filepath=file_path)
-    return matches
